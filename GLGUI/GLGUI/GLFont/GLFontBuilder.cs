73e8a99422770d183684df78c99192f822c3fe03
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-27 10:17:23 +0800

    clean

--- GLGUI/GLGUI/GLFont/GLFontBuilder.cs
+++ GLGUI/GLGUI/GLFont/GLFontBuilder.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace GLGUI
 {
-    class GLFontBuilder
+    partial class GLFontBuilder
     {
         private string charSet;
         private GLFontBuilderConfiguration config;
@@ -77,50 +77,6 @@ namespace GLGUI
             return false;
         }
 
-        //The initial bitmap is simply a long thin strip of all glyphs in a row
-        private Bitmap CreateInitialBitmap(Font font, SizeF maxSize, int initialMargin, out GLFontGlyph[] glyphs, GLFontRenderHint renderHint)
-        {
-            glyphs = new GLFontGlyph[charSet.Length];
-
-            int spacing = (int)Math.Ceiling(maxSize.Width) + 2 * initialMargin;
-            Bitmap bmp = new Bitmap(spacing * charSet.Length, (int)Math.Ceiling(maxSize.Height) + 2 * initialMargin + 1, PixelFormat.Format24bppRgb);
-            Graphics graph = Graphics.FromImage(bmp);
-
-            switch (renderHint)
-            {
-                case GLFontRenderHint.SizeDependent:
-                    graph.TextRenderingHint = font.Size <= 12.0f ? TextRenderingHint.ClearTypeGridFit : TextRenderingHint.AntiAlias;
-                    break;
-                case GLFontRenderHint.AntiAlias:
-                    graph.TextRenderingHint = TextRenderingHint.AntiAlias;
-                    break;
-                case GLFontRenderHint.AntiAliasGridFit:
-                    graph.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-                    break;
-                case GLFontRenderHint.ClearTypeGridFit:
-                    graph.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
-                    break;
-                case GLFontRenderHint.SystemDefault:
-                    graph.TextRenderingHint = TextRenderingHint.SystemDefault;
-                    break;
-            }
-
-            int xOffset = initialMargin;
-            for (int i = 0; i < charSet.Length; i++)
-            {
-                graph.DrawString("" + charSet[i], font, Brushes.White, xOffset, initialMargin);
-                var charSize = graph.MeasureString("" + charSet[i], font);
-                glyphs[i] = new GLFontGlyph(0, new Rectangle(xOffset - initialMargin, 0, (int)charSize.Width + initialMargin * 2, (int)charSize.Height + initialMargin * 2), 0, charSet[i]);
-                xOffset += (int)charSize.Width + initialMargin * 2;
-            }
-
-            graph.Flush();
-            graph.Dispose();
-
-            return bmp;
-        }
-
-        private delegate bool EmptyDel(BitmapData data, int x, int y);
 
         private static void RetargetGlyphRectangleInwards(BitmapData bitmapData, GLFontGlyph glyph, bool setYOffset, byte alphaTolerance)
         {
