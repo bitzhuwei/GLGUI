3dd27b1e40dd65619e6922ea56691207acae5af7
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-10 16:40:01 +0800

    only one text box.

--- GLGUI/GLGUI.GLControlExample/GuiControl.cs
+++ GLGUI/GLGUI.GLControlExample/GuiControl.cs
@@ -35,9 +35,9 @@ namespace GLGUI.GLControlExample
 
         private void OnLoad(object sender, EventArgs e)
         {
-            VSync = false; // vsync is nice, but you can't really measure performance while it's on
+            this.VSync = false; // vsync is nice, but you can't really measure performance while it's on
 
-            glgui = new GLGui(this);
+            this.glgui = new GLGui(this);
 
             var mainAreaControl = glgui.Add(new GLGroupLayout(glgui) { Size = new Size(ClientSize.Width, ClientSize.Height - 200), Anchor = GLAnchorStyles.All });
             // change background color:
@@ -56,17 +56,6 @@ namespace GLGUI.GLControlExample
             skin.BackgroundColor = glgui.Skin.TextBoxActive.BackgroundColor;
             fpsLabel.SkinEnabled = skin;
 
-            var helloWorldForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Hello World", Outer = new Rectangle(50, 100, 200, 150), AutoSize = false });
-            helloWorldForm.Add(new GLForm(glgui) { Title = "Hello Form", Outer = new Rectangle(100, 32, 100, 100), AutoSize = false })
-                .MouseMove += (s, w) => Console.WriteLine(w.Position.ToString());
-
-            var flow = helloWorldForm.Add(new GLFlowLayout(glgui) { FlowDirection = GLFlowDirection.BottomUp, Location = new Point(10, 10), Size = helloWorldForm.InnerSize, AutoSize = true });
-            for (int i = 0; i < 5; i++)
-                flow.Add(new GLButton(glgui) { Text = "Button" + i, Size = new Size(150, 0) })
-                    .Click += (s, w) => Console.WriteLine(s + " pressed.");
-            flow.Add(new GLButton(glgui) { Text = "Hide Cursor", Size = new Size(150, 0) })
-                .Click += (s, w) => glgui.Cursor = GLCursor.None;
-
             var loremIpsumForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Lorem Ipsum", Location = new Point(600, 100), Size = new Size(300, 200) });
             loremIpsumForm.Add(new GLTextBox(glgui)
             {
@@ -77,56 +66,38 @@ namespace GLGUI.GLControlExample
                 Anchor = GLAnchorStyles.All
             }).Changed += (s, w) => Console.WriteLine(s + " text length: " + ((GLTextBox)s).Text.Length);
 
-            var fixedSizeForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Fixed size Form", Location = new Point(64, 300), Size = new Size(100, 200), AutoSize = true });
-            var fooBarFlow = fixedSizeForm.Add(new GLFlowLayout(glgui) { FlowDirection = GLFlowDirection.TopDown, AutoSize = true });
-            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "CheckBox A", AutoSize = true })
-                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
-            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "CheckBox B", AutoSize = true })
-                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
-            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "Totally different CheckBox", AutoSize = true })
-                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
-            fooBarFlow.Add(new GLCheckBox(glgui) { Text = "Go away!", AutoSize = true })
-                .Changed += (s, w) => Console.WriteLine(s + ": " + ((GLCheckBox)s).Checked);
+            this.stopwatch = new Stopwatch();
+            this.Resize += (s, ev) => GL.Viewport(ClientSize);
+            this.Paint += OnRender;
 
-            for (int i = 0; i < 3; i++)
-            {
-                var viewportForm = mainAreaControl.Add(new GLForm(glgui) { Title = "Cube", Location = new Point(300 + i * 16, 64 + i * 16), Size = new Size(200, 200) });
-                viewportForm.Add(new GLViewport(glgui) { Size = viewportForm.InnerSize, Anchor = GLAnchorStyles.All })
-                    .RenderViewport += OnRenderViewport;
-            }
-
-            stopwatch = new Stopwatch();
-            Resize += (s, ev) => GL.Viewport(ClientSize);
-            Paint += OnRender;
-
-            stopwatch.Start();
+            this.stopwatch.Start();
             Application.Idle += (s, ev) => Invalidate();
         }
 
         private void OnRender(object sender, PaintEventArgs e)
         {
-            stopwatch.Stop();
-            double delta = stopwatch.Elapsed.TotalMilliseconds * 0.001;
-            stopwatch.Restart();
-            time += delta;
+            this.stopwatch.Stop();
+            double delta = this.stopwatch.Elapsed.TotalMilliseconds * 0.001;
+            this.stopwatch.Restart();
+            this.time += delta;
 
-            if (time >= fpsSecond)
+            if (this.time >= this.fpsSecond)
             {
                 fpsLabel.Text = string.Format("Application: {0:0}FPS. GLGUI: {1:0.0}ms", fpsCounter, glgui.RenderDuration);
                 fpsCounter = 0;
                 fpsSecond++;
             }
 
-            if (consoleWriter.Changed)
+            if (this.consoleWriter.Changed)
             {
-                console.Text = string.Join("\n", consoleWriter.Lines);
-                consoleWriter.Changed = false;
+                this.console.Text = string.Join("\n", this.consoleWriter.Lines);
+                this.consoleWriter.Changed = false;
             }
 
-            glgui.Render();
+            this.glgui.Render();
             SwapBuffers();
 
-            fpsCounter++;
+            this.fpsCounter++;
         }
 
         // draws a simple colored cube in a GLViewport control
