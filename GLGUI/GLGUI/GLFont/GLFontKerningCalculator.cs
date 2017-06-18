2f9dd7fe5554105da2f27b0f0050a77b06bb8b64
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-18 20:51:00 +0800

    update

--- GLGUI/GLGUI/GLFont/GLFontKerningCalculator.cs
+++ GLGUI/GLGUI/GLFont/GLFontKerningCalculator.cs
@@ -11,40 +11,6 @@ namespace GLGUI
             public int Max;
         }
 
-        private static int Kerning(GLFontGlyph g1, GLFontGlyph g2, XLimits[] lim1, XLimits[] lim2, GLFontKerningConfiguration config)
-        {
-            int yOffset1 = g1.YOffset;
-            int yOffset2 = g2.YOffset;
-
-            int startY = Math.Max(yOffset1, yOffset2);
-            int endY = Math.Min(g1.Rect.Height + yOffset1, g2.Rect.Height + yOffset2);
-
-            int w1 = g1.Rect.Width;
-
-            int worstCase = w1;
-
-            //TODO - offset startY, endY by yOffset1 so that lim1[j-yOffset1] can be written as lim1[j], will need another var for yOffset2
-
-            for (int j = startY; j < endY; j++)
-                worstCase = Math.Min(worstCase, w1 - lim1[j-yOffset1].Max + lim2[j-yOffset2].Min);
-
-            worstCase = Math.Min(worstCase, g1.Rect.Width);
-            worstCase = Math.Min(worstCase, g2.Rect.Width);
-
-            //modify by character kerning rules
-            GLFontCharacterKerningRule kerningRule = config.GetOverridingCharacterKerningRuleForPair(""+g1.Character + g2.Character);
-            if (kerningRule == GLFontCharacterKerningRule.Zero)
-            {
-                return 0;
-            }
-            else if (kerningRule == GLFontCharacterKerningRule.NotMoreThanHalf)
-            {
-                return (int)Math.Min(Math.Min(g1.Rect.Width,g2.Rect.Width)*0.5f, worstCase);
-            }
-
-            return worstCase;
-        }
-
         public static Dictionary<String, int> CalculateKerning(char[] charSet, GLFontGlyph[] glyphs, List<GLFontBitmap> bitmapPages, GLFontKerningConfiguration config)
         {
             var kerningPairs = new Dictionary<String, int>();
@@ -73,7 +39,7 @@ namespace GLGUI
                     bool yetToFindFirst = true;
                     for (int i = xStart; i < xEnd; i++)
                     {
-                        if (!GLFontBitmap.EmptyAlphaPixel(page.bitmapData, i, j,config.AlphaEmptyPixelTolerance))
+                        if (!GLFontBitmap.EmptyAlphaPixel(page.bitmapData, i, j, config.AlphaEmptyPixelTolerance))
                         {
 
                             if (yetToFindFirst)
@@ -103,7 +69,8 @@ namespace GLGUI
 
                 for (int j = 0; j < limits[n].Length; j++)
                 {
-                    if(j != 0){
+                    if (j != 0)
+                    {
                         tmp[j].Min = Math.Min(limits[n][j - 1].Min, tmp[j].Min);
                         tmp[j].Max = Math.Max(limits[n][j - 1].Max, tmp[j].Max);
                     }
@@ -113,7 +80,7 @@ namespace GLGUI
                         tmp[j].Min = Math.Min(limits[n][j + 1].Min, tmp[j].Min);
                         tmp[j].Max = Math.Max(limits[n][j + 1].Max, tmp[j].Max);
                     }
-                    
+
                 }
 
                 for (int j = 0; j < limits[n].Length; j++)
@@ -123,9 +90,44 @@ namespace GLGUI
 
             for (int i = 0; i < charSet.Length; i++)
                 for (int j = 0; j < charSet.Length; j++)
-                    kerningPairs.Add("" + charSet[i] + charSet[j], 1-Kerning(glyphs[i], glyphs[j], limits[i], limits[j],config));
+                    kerningPairs.Add("" + charSet[i] + charSet[j], 1 - Kerning(glyphs[i], glyphs[j], limits[i], limits[j], config));
 
             return kerningPairs;
         }
+
+        private static int Kerning(GLFontGlyph g1, GLFontGlyph g2, XLimits[] lim1, XLimits[] lim2, GLFontKerningConfiguration config)
+        {
+            int yOffset1 = g1.YOffset;
+            int yOffset2 = g2.YOffset;
+
+            int startY = Math.Max(yOffset1, yOffset2);
+            int endY = Math.Min(g1.Rect.Height + yOffset1, g2.Rect.Height + yOffset2);
+
+            int w1 = g1.Rect.Width;
+
+            int worstCase = w1;
+
+            //TODO - offset startY, endY by yOffset1 so that lim1[j-yOffset1] can be written as lim1[j], will need another var for yOffset2
+
+            for (int j = startY; j < endY; j++)
+                worstCase = Math.Min(worstCase, w1 - lim1[j - yOffset1].Max + lim2[j - yOffset2].Min);
+
+            worstCase = Math.Min(worstCase, g1.Rect.Width);
+            worstCase = Math.Min(worstCase, g2.Rect.Width);
+
+            //modify by character kerning rules
+            GLFontCharacterKerningRule kerningRule = config.GetOverridingCharacterKerningRuleForPair("" + g1.Character + g2.Character);
+            if (kerningRule == GLFontCharacterKerningRule.Zero)
+            {
+                return 0;
+            }
+            else if (kerningRule == GLFontCharacterKerningRule.NotMoreThanHalf)
+            {
+                return (int)Math.Min(Math.Min(g1.Rect.Width, g2.Rect.Width) * 0.5f, worstCase);
+            }
+
+            return worstCase;
+        }
+
     }
 }
